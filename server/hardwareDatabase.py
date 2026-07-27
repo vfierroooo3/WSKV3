2907f82b5fab8068298f360cb8c8d1bda0996ccc
Valentina Fierro <vfierro223@gmail.com>
2026-07-27 15:13:46 -0500

    Actuall Hardware Functions, Other one was Wrong

--- server/hardwareDatabase.py
+++ server/hardwareDatabase.py
@@ -1,5 +1,6 @@
 # Import necessary libraries and modules
 from pymongo import MongoClient
+import databaseHelpers
 
 '''
 Structure of Hardware Set entry:
@@ -10,11 +11,16 @@ HardwareSet = {
 }
 '''
 
+def access_collection(client, collection_name):
+    '''Enter Collection Name to Access From Database'''
+    db = client["WSKV3"]
+    return db[collection_name]
+
 # Function to create a new hardware set
 def createHardwareSet(client, hwSetName, initCapacity):
     # Create a new hardware set in the database
-        db = client.WSKV3
-        collection = db.HardwareSets
+        
+        collection = access_collection(client, "HardwareSets")
 
         hardwareSet = {
              "hwName": hwSetName,
@@ -30,15 +36,13 @@ def createHardwareSet(client, hwSetName, initCapacity):
 # Function to query a hardware set by its name
 def queryHardwareSet(client, hwSetName):
     # Query and return a hardware set from the database
-    db = client.WSKV3
-    collection = db.HardwareSets
+    collection = access_collection(client, "HardwareSets")
     return collection.find_one({"hwName": hwSetName})
 
 # Function to update the availability of a hardware set
 def updateAvailability(client, hwSetName, newAvailability):
     # Update the availability of an existing hardware set
-    db = client.WSKV3
-    collection = db.HardwareSets
+    collection = access_collection(client, "HardwareSets")
     result = collection.update_one(
          {"hwName": hwSetName},
          {"$set": {"availability": newAvailability}}
@@ -48,15 +52,19 @@ def updateAvailability(client, hwSetName, newAvailability):
 # Function to request space from a hardware set
 def requestSpace(client, hwSetName, amount):
     # Request a certain amount of hardware and update availability
-    db = client.WSKV3
-    collection = db.HardwareSets
+    collection = access_collection(client, "HardwareSets")
     hardware_set = collection.find_one({"hwName": hwSetName})
-    return hardware_set and hardware_set['availability'] >= amount
+    if not hardware_set or hardware_set ['availability'] < amount:
+         return False # Not enough availability or hardware set does not exist
+    result = collection.update_one(
+         {"hwName": hwSetName},
+         {"$inc": {"availability": -amount}}
+    )
+    return result.modified_count > 0  # Return True if the update was successful, False otherwise
 
 # Function to get all hardware set names
 def getAllHwNames(client):
     # Get and return a list of all hardware set names
-    db = client.WSKV3
-    collection = db.HardwareSets
+    collection = access_collection(client, "HardwareSets")
     return [hw['hwName'] for hw in collection.find({}, {"_id": 0, "hwName": 1})]
 
