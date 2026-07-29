311cac25257eff8b9646c0b3969b81c15e5388f5
Wei <weitingyu1127@gmail.com>
2026-07-29 16:53:36 -0500

    fix getElementById

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -7,9 +7,9 @@ function LoginPage() {
 
   // Check login credentials
   function checkLogin() {
-    const userId = document.querySelector('input[placeholder="Please Enter UserId"]').value;
-    const password = document.querySelector('input[placeholder="Please Enter Password"]').value;
-    const errorMessage = document.getElementById("error-message");
+    const userId = document.getElementById("userId").value;
+    const password = document.getElementById("password").value;
+    const errorMessage = document.getElementById("errorMessage");
     
     if (!userId || !password) {
       errorMessage.innerText = "Please fill in all fields!";
@@ -25,7 +25,9 @@ function LoginPage() {
       const result = await sharedApi("/login", "POST", { userId, password });
 
       if (result.success) {
-        navigate("/main-page", { state: { userId: userId } });
+        localStorage.setItem("isLogin", "true");
+        localStorage.setItem("userId", userId);
+        navigate("/main-page");
       } else {
         errorMessage.innerText = result.message;
       }
@@ -33,20 +35,20 @@ function LoginPage() {
       console.error("Error during login:", error);
       errorMessage.innerText = "An error occurred during login. Please try again.";
     }
-    }
+  }
   return (
     <main style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "24px"}}>
       <h1>Login</h1>
         <div style={{ display: "flex", flexDirection: "column", gap: "12px", width: "300px"  }}>
           <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
-            UserId: <input type="text" id="user-id" placeholder="Please Enter UserId" />
+            UserId: <input type="text" id="userId" placeholder="Please Enter UserId" />
           </div>
 
           <div style={{ display: "flex", flexDirection: "column", gap: "4px" }}>
             Password: <input type="password" id="password" placeholder="Please Enter Password" />
           </div>
 
-          <span style={{ color: "red" }} id="error-message"></span>
+          <span style={{ color: "red" }} id="errorMessage"></span>
 
           <button type="button" onClick={checkLogin} className="custom-button">
             Login
