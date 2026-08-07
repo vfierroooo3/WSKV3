9f807ae5013fd1c95e80e13143775f0e422bc15d
Wei <weitingyu1127@gmail.com>
2026-08-06 23:19:34 -0500

    fix: move the DB url to databaseHelpers.py

--- server/databaseHelpers.py
+++ server/databaseHelpers.py
@@ -1,12 +1,8 @@
-import os
-from dotenv import load_dotenv
 from pymongo import MongoClient
 
-# Load environment variables from .env file
-load_dotenv() 
-
 # Get MongoDB server address from environment variable
-MONGODB_SERVER = os.getenv("MONGODB_SERVER")
+# Define the MongoDB connection string
+MONGODB_SERVER = "mongodb+srv://katierafdahl_db_user:45sfmVPrbATWYm3t@wskv3.zcpyq8u.mongodb.net/?appName=WSKV3"
 
 # Connect to MongoDB
 client = MongoClient(MONGODB_SERVER)
